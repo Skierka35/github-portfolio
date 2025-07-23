a8cf1c3b02c1080a569b931a9a4e6f29e2213a59
skierka <Julia.Koszczol112@gmail.com>
2025-07-23 16:54:31 +0200

    Contact site update

--- portfolio-react/src/app/components/contact.tsx
+++ portfolio-react/src/app/components/contact.tsx
@@ -3,16 +3,28 @@ import Image from 'next/image';
 export default function ContactPage() {
   return (
     <section className="text-center text-gray-200 ">
-      <p>Email: <a href="mailto:Julka.Koszczol112@gmail.com" className="text-purple-400 underline">Julia.Koszczol112@gmail.com</a></p>
+      <p>Email:{" "} 
+      <a 
+        href="mailto:Julia.Koszczol112@gmail.com" 
+        className="text-purple-400 hover:underline"
+        >
+      Julia.Koszczol112@gmail.com</a>
+      </p>
       <p>Numer telefonu: +48 508 617 676 </p>
       <br></br>
-      <Image
-      src="/github-logo.png" 
-      width={60} 
-      height={60} 
-      alt="https://github.com/Skierka35"
-      className="mx-auto"
-      />
+      <a 
+        href="https://github.com/Skierka35/github-portfolio" 
+        target="_blank" 
+        rel="noopener noreferrer"
+      >
+        <Image
+          src="/github-logo.png"
+          width={60}
+          height={60}
+          alt="GitHub logo"
+          className="mx-auto cursor-pointer transition hover:scale-110"
+        />
+      </a>
     </section>
   );
 }

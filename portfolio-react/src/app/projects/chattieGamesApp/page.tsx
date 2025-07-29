5f42334279943817c0665eb717a516249385c9f0
skierka <Julia.Koszczol112@gmail.com>
2025-07-29 10:20:51 +0200

    test page commit.

---
+++ portfolio-react/src/app/projects/chattieGamesApp/page.tsx
@@ -0,0 +1,10 @@
+import NightSky from '../../components/nightSky'
+
+export default function projectsPage() {
+  return (
+    <section className=" relative h-screen p-6 text-white">
+      <NightSky />
+      <h1 className="text-2xl font-bold mb-4">Test Chattie</h1>
+    </section>
+  );
+}
--- portfolio-react/src/app/projects/portfolioInfo/page.tsx
+++ portfolio-react/src/app/projects/portfolioInfo/page.tsx
@@ -1,12 +1,10 @@
-import Gallery from '../../components/projectGallery'
 import NightSky from '../../components/nightSky'
 
 export default function projectsPage() {
   return (
     <section className=" relative h-screen p-6 text-white">
       <NightSky />
-      <h1 className="text-2xl font-bold mb-4">Projekty</h1>
-      <Gallery />
+      <h1 className="text-2xl font-bold mb-4">Test portfolio</h1>
     </section>
   );
 }
